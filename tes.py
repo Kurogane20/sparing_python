47f36d2a1deb950cbae2bde4ccc29a72582b29f6
Kurogane20 <nurkhalis4@gmail.com>
2026-03-01 13:50:09 +0700

    tes sensor

--- tes.py
+++ tes.py
@@ -2,7 +2,6 @@ import asyncio
 from pymodbus.client import AsyncModbusSerialClient
 
 async def main():
-
     client = AsyncModbusSerialClient(
         port="/dev/ttyUSB0",
         baudrate=9600,
@@ -14,17 +13,16 @@ async def main():
 
     await client.connect()
 
-    # baca slave 2 register 0 sebanyak 2
     result = await client.read_holding_registers(
         address=0,
         count=2,
-        slave=2
+        device_id=2   # ← ini yang benar
     )
 
     if result.isError():
         print("Gagal baca")
     else:
-        print("Data:", result.registers)
+        print("Register:", result.registers)
 
     await client.close()
 
